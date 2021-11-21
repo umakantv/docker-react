2e9707f4524a1c7375734476d0a5308d3c96c331
Umakant Vashishtha <umakantvashishtha98@gmail.com>
2021-11-21 20:41:00 +0530

    Keep react link

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello from Umakant.
+          Hi there!
         </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
